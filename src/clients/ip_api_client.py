e35154f9b4a84be7409e6af5aaf20ab3bcc96871
Saveliy Efimov <savva.cat@mail.ru>
2026-03-02 00:21:46 +0300

    minor changes

--- src/clients/ip_api_client.py
+++ src/clients/ip_api_client.py
@@ -1,6 +1,8 @@
 from enum import Enum
+from typing import Any
 
 from src.clients.base_http_client import BaseHTTPClient
+from src.common.constants import IP_API_HOST
 
 
 class IpApiLanguage(Enum):
@@ -14,14 +16,14 @@ class IpApiLanguage(Enum):
     CHINESE = 'zh-CN'
 
 class IpApiClient(BaseHTTPClient):
-    BASE_URL = 'http://ip-api.com/json'
+    BASE_URL = f'http://{IP_API_HOST}/json'
 
     async def get_ip_info(
         self,
         ip: str,
         language: IpApiLanguage = IpApiLanguage.ENGLISH,
         fields: list[str] | None = None,
-    ):
+    ) -> dict[str, Any]:
         async with self.session.get(
             url=f'{self.BASE_URL}/{ip}',
             params={
@@ -31,3 +33,16 @@ class IpApiClient(BaseHTTPClient):
         ) as response:
             response.raise_for_status()
             return await response.json()
+
+
+if __name__ == '__main__':
+    client = IpApiClient()
+    import asyncio
+
+    async def test() -> None:
+        async with client:
+            result = await client.get_ip_info('8.8.8.8')
+
+        print(result)
+
+    asyncio.run(test())
